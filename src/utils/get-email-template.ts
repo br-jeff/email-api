7640187202d2d25a277fe75153c734bba2a380a7
Jefferson Silva <jefferson_1996Th@hotmail.com>
2024-02-18 19:01:46 -0300

    get email template from path

---
+++ src/utils/get-email-template.ts
@@ -0,0 +1,17 @@
+import fs from 'fs'
+import path from 'path'
+import util from 'util'
+
+import _ from 'lodash'
+
+const readFile = util.promisify(fs.readFile)
+
+export async function getEmailHtml(
+  data: Record<string, unknown>,
+  templatePath: string,
+): Promise<string> {
+  const dir = path.resolve(templatePath)
+  const template = await readFile(dir, 'utf-8')
+  const compiled = _.template(template)
+  return compiled(data)
+}
